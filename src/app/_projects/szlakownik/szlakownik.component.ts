3a1193ac406511eaa0ab3205b2cb0013a9c5a863
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-02-10 19:01:01 +0100

    Uzupełnienie szlakownika i przesunięcie przycisku z linkiem do gh

--- src/app/_projects/szlakownik/szlakownik.component.ts
+++ src/app/_projects/szlakownik/szlakownik.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-szlakownik',
   templateUrl: './szlakownik.component.html',
-  styleUrls: ['./szlakownik.component.css']
+  styleUrls: ['./szlakownik.component.css', '../../../project-styles.css']
 })
 export class SzlakownikComponent implements OnInit {
 
