0f44d0692c7cf33567b3e500050a81b24a4de49c
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-02-11 13:36:05 +0100

    Plant Runner

--- src/app/_projects/plant-runner/plant-runner.component.ts
+++ src/app/_projects/plant-runner/plant-runner.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-plant-runner',
   templateUrl: './plant-runner.component.html',
-  styleUrls: ['./plant-runner.component.css']
+  styleUrls: ['./plant-runner.component.css', '../project-styles.css']
 })
 export class PlantRunnerComponent implements OnInit {
 
