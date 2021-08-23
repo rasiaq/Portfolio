29d85934364a58f9c427f484e7ee3b378a794b35
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-08-23 11:55:48 +0200

    cards update

--- src/app/projects-cards/projects-cards.component.ts
+++ src/app/projects-cards/projects-cards.component.ts
@@ -13,48 +13,42 @@ export class ProjectsCardsComponent implements OnInit {
       icon: '../../assets/astroview-img/icon.png', title: 'AstroView',
       description: 'Aplikacja na platformę Android do sprawdzania horoskopu, pobierająca dane po API.',
       link: 'projects/astroview',
-      techs: 'Android, Java',
-      status: 'Ukończony'
+      techs: 'Android, Java'
     },
     {
       icon: '../../assets/portfolio/icon.png',
       title: 'Portfolio',
       description: 'Responsywna strona internetowa, napisana w Angularze, która obecnie jest moim portfolio.',
       link: 'projects/portfolio',
-      techs: 'Angular',
-      status: 'Ukończony'
+      techs: 'Angular'
     },
     {
       icon: '../../assets/szlakownik/icon.png',
       title: 'Szlakownik',
       description: 'Zespołowy projekt aplikacji webowej dla miłośników turystyki górskiej!',
       link: 'projects/szlakownik',
-      techs: 'Angular, Python, Flask',
-      status: 'Ukończony'
+      techs: 'Angular, Python, Flask'
     },
     {
       icon: '../../assets/law-firm-web/icon.png',
       title: 'Strona kancelarii',
       description: 'Zespołowy projekt strony internetowej w Angularze dla kancelarii prawnej.',
       link: 'projects/law-firm-website',
-      techs: 'Angular, PrimeNG',
-      status: 'Ukończony'
+      techs: 'Angular, PrimeNG'
     },
     {
       icon: '../../assets/plantrunner/icon.png',
       title: 'Plant Runner',
       description: 'Hybrydowa aplikacja mobilna, zachęcająca użytkowników do przemieszczania się pieszo.',
       link: 'projects/plant-runner',
-      techs: 'Ionic, Angular',
-      status: 'W trakcie'
+      techs: 'Ionic'
     },
     {
       icon: '../../assets/netalyze/icon.png',
       title: 'Strona Netalyze',
       description: 'Strona wydziałowego koła naukowego Netalyze na Politechnice Wrocławskiej.',
       link: 'projects/netalyze-website',
-      techs: 'Angular',
-      status: 'W fazie projektowania'
+      techs: 'Angular'
     },
   ]
 
